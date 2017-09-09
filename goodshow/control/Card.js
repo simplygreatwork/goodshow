8f019ad877c296a66430d1d2040706ac18b0aad6
Philip Weaver <simplygreatwork@gmail.com>
2017-09-09 09:57:49 +0000

    pushing

--- goodshow/control/Card.js
+++ goodshow/control/Card.js
@@ -10,23 +10,24 @@ goodshow.Card = goodshow.Panel.extend({
 				arranger : new goodshow.arranger.Stack(),
 				children : [
 					this.graphics = new goodshow.Panel({
-						background : 0xFFFFFF
+						background : 0xFFFFFF,
+						filter : {
+							filters : [Object.assign(new PIXI.filters.DropShadowFilter(), {
+								color : 0x888888,
+								alpha : 0.4,
+								blur : 4,
+								distance : 5
+							})]
+						}
 					}),
 					this.panel = new goodshow.Panel(Object.assign({}, options || {})),
 				]
-			},
+			}
 		});
 	},
 	
 	draw: function() {
 		
 		goodshow.Panel.prototype.draw.call(this);
-		this.graphics.filters = [Object.assign(new PIXI.filters.DropShadowFilter(), {
-			color : 0x888888,
-			alpha : 0.4,
-			blur : 4,
-			distance : 5
-		})];
-		this.panel.draw();
 	}
 });
